bf831240bacf638b399545449012705d38ba43cb
Kerri-Ann <kaybee1jam@yahoo.com>
2020-04-21 16:35:26 -0400

    dynamic program completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,22 +20,34 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
+        program_filename = sys.argv[1]
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8 -> store 8 in R0
-            0b00000000,  # represent R0
-            0b00001000,  # represent the value 8
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8 -> store 8 in R0
+        #     0b00000000,  # represent R0
+        #     0b00001000,  # represent the value 8
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        with open(program_filename) as f:
+            for line in f:
+                line = line.split("#")
+                line = line[0].strip()
+
+                if line == "":
+                    continue
+
+                self.ram[address] = int(line, 2)
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -102,4 +114,4 @@ class CPU:
                 print("Uknown instruction")
                 self.running = False
 
-        self.trace()
+        # self.trace()
