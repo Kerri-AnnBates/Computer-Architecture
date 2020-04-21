61c83ddd8c0b4390bfc59af7a0d026d98edb4701
Kerri-Ann <kaybee1jam@yahoo.com>
2020-04-20 20:49:42 -0400

    Day 1: Get print8.ls8 running

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,19 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8  # R0 - R7
+        self.pc = 0
+        self.running = True
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
 
     def load(self):
         """Load a program into memory."""
@@ -18,48 +25,81 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8 -> store 8 in R0
+            0b00000000,  # represent R0
+            0b00001000,  # represent the value 8
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
         """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+		Handy function to print out the CPU state. You might want to call this
+		from run() if you need help debugging.
+		"""
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |"
+            % (
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2),
+            ),
+            end="",
+        )
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.reg[i], end="")
 
         print()
 
+    def ram_read(self, MAR):
+        """Should accept the address to read and return the value stored there"""
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        """Should accept a value to write, and the address to write it to."""
+        self.ram[MAR] = MDR
+
     def run(self):
         """Run the CPU."""
-        pass
+        PC = self.pc
+
+        while self.running:
+            # read the memory address that's stored in register PC and store that result in IR, Instruction Register
+            IR = self.ram[PC]
+
+            if IR == self.LDI:
+                reg_num = self.ram_read(PC + 1)
+                value = self.ram_read(PC + 2)
+                self.ram_write(value, reg_num)
+                PC += 3
+            elif IR == self.PRN:
+                reg_num = self.ram_read(PC + 1)
+                value = self.ram_read(reg_num)
+                print(value)
+                PC += 2
+            elif IR == self.HLT:
+                self.running = False
+            else:
+                print("Uknown instruction")
+                self.running = False
+
+        self.trace()
